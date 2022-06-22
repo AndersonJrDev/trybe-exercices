7e51bfec9c1ab769b26544aad4bf8a355d9dd3f4
Anderson <andersonrany8@gmail.com>
2022-06-22 15:25:28 -0300

    correção da branch

--- fundamentos/bloco-4/4.1/estudo4.js
+++ fundamentos/bloco-4/4.1/estudo4.js
@@ -17,8 +17,8 @@ if (currentHour >= 22){
     console.log(message)
 }else{}
 
-var weekDay = "quarta";
-if (weekDay = "segunda-feira" || "terça-feira" || "quarta-feira" || "quinta-feira" || "sexta-feira"){
+var weekDay = "quarta-feira";
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira"){
     console.log("Oba, mais um dia de aprendizado na Trybe >:D")
 }else{
     console.log("FINALMENTE, descanso merecido UwU")
